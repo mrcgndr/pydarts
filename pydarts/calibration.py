44afec6e1d79544e9fdcc8a3b65d1a0b000fc40e
Maurice Günder <mauriceguender@yahoo.de>
2022-01-30 16:29:10 +0100

    better ellipse detection; several improvements

--- pydarts/calibration.py
+++ pydarts/calibration.py
@@ -1,11 +1,19 @@
-from dataclasses import dataclass
-from typing import List, Union, Tuple
-import numpy as np
-import cv2
 import itertools as it
-from shapely.geometry import MultiLineString, LineString, Point, MultiPoint
+from dataclasses import dataclass
+from typing import List, Tuple, Union
+
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.ndimage as ndi
 from scipy.optimize import minimize
+from scipy.spatial import ConvexHull
+from shapely.geometry import LineString, MultiLineString, MultiPoint, Point
+
 from util import rotate_anchor
+
+
+FIGSIZE = (20,12)
 
 
 @dataclass
@@ -24,10 +32,19 @@ class Ellipse():
 
     def get_edge_points(self) -> np.ndarray:
         a = np.deg2rad(self.ang)
-        return np.array([[self.x-np.cos(a)*self.b/2, self.y-np.sin(a)*self.b/2],  # top
-                         [self.x+np.sin(a)*self.a/2, self.y-np.cos(a)*self.a/2],  # right
-                         [self.x+np.cos(a)*self.b/2, self.y+np.sin(a)*self.b/2],  # bottom
-                         [self.x-np.sin(a)*self.a/2, self.y+np.cos(a)*self.a/2]]) # left
+        return np.array([
+            [self.x-np.cos(a)*self.b/2, self.y-np.sin(a)*self.b/2], # top
+            [self.x+np.sin(a)*self.a/2, self.y-np.cos(a)*self.a/2], # right
+            [self.x+np.cos(a)*self.b/2, self.y+np.sin(a)*self.b/2], # bottom
+            [self.x-np.sin(a)*self.a/2, self.y+np.cos(a)*self.a/2]  # left
+        ])
+
+    def get_limits(self) -> Tuple[Tuple[float, float]]:
+        a = np.deg2rad(self.ang)
+        return (
+            (self.x-np.sin(a)*self.a/2, self.x+np.sin(a)*self.a/2),
+            (self.y-np.sin(a)*self.b/2, self.y+np.sin(a)*self.b/2)
+        )
 
     def transform_edge_points(self, M: np.ndarray) -> np.array:
         t = (M@np.column_stack((self.get_edge_points(), np.ones(4))).T).T
@@ -57,12 +74,13 @@ class BullsEye():
 
 class Calibration():
 
-    def __init__(self, img) -> None:
-        self.image = img
-        if len(img.shape) > 2:
-            self.h, self.w, self.c = img.shape
+    def __init__(self, image: np.ndarray, debug_plots: bool = False) -> None:
+        self.image = image
+        self.debug_plots = debug_plots
+        if len(image.shape) > 2:
+            self.h, self.w, self.c = image.shape
         else:
-            self.h, self.w = img.shape
+            self.h, self.w = image.shape
             self.c = None
         self.ellipse = Ellipse()
         self.bullseye = BullsEye()
@@ -70,12 +88,20 @@ class Calibration():
     def preprocess_ellipse(self) -> np.ndarray:
         # convert to HSV
         hsv = cv2.cvtColor(self.image, cv2.COLOR_BGR2HSV)
+        calib_img = np.uint8((hsv[:,:,1] > 120)*255)
+        """
         # use saturation channel for binary thresholding
         _, thresh = cv2.threshold(hsv[:,:,1], 120, 255, cv2.THRESH_BINARY)
         # remove radial wires
         kernel_size = 10
         kernel = np.ones((kernel_size, kernel_size), np.float32) / kernel_size**2
         calib_img = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
+        """
+        if self.debug_plots:
+            fig, ax = plt.subplots(figsize=FIGSIZE)
+            ax.imshow(calib_img)
+            ax.set(title="preprocessed image\nfor ellipse detection")
+            fig.show()
         return calib_img
 
     def ellipse_mask(self, image: np.ndarray) -> np.ndarray:
@@ -89,23 +115,27 @@ class Calibration():
         # preprocess image
         calib_img = self.preprocess_ellipse()
         # find contours
-        contours, hierarchy = cv2.findContours(calib_img, 1, 2)
+        distance = ndi.distance_transform_edt(calib_img)
+        edges = cv2.Canny(np.uint8((distance > 10)*255), 0, 10)
+        contours, hierarchy = cv2.findContours(edges, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_TC89_KCOS)
+        # get convex hull points
+        P = np.vstack(contours)[:,0,:]
+        hull = ConvexHull(P)
+        P_hull = np.vstack(P[hull.simplices])
         # fit ellipses
-        ellipses = []
-        areas = []
-        for cnt in contours:
-            try:
-                ell = cv2.fitEllipse(cnt)
-                # valid ellipses have centers inside the image
-                if (0 < ell[0][0] < self.image.shape[0]) and (0 < ell[0][1] < self.image.shape[1]):
-                    ellipses.append(ell)
-                    areas.append(cv2.contourArea(cnt))
-            except:
-                pass
-        if len(ellipses) == 0:
+        try:
+            ellipse = cv2.fitEllipse(P_hull)
+        except:
             raise("No calibration ellipse found. Please check the webcam.")
         # set ellipse with largest area as calibration ellipse
-        ellipse = ellipses[np.argmax(areas)]
+        if self.debug_plots:
+            fig, ax = plt.subplots(figsize=FIGSIZE)
+            img = self.image.copy()
+            ell_img = cv2.ellipse(img, ellipse, (0,255,255), 10)
+            ax.imshow(ell_img[:,:,::-1])
+            ax.scatter(*P_hull.T, c="y")
+            ax.set(title="detected ellipse")
+            fig.show()
         self.ellipse = Ellipse(
                             x=ellipse[0][0], y=ellipse[0][1],
                             a=ellipse[1][1], b=ellipse[1][0],
@@ -127,7 +157,7 @@ class Calibration():
         # preprocess image
         canny = self.preprocess_wires()
         # Hough line detection
-        lines = cv2.HoughLines(canny, rho=2, theta=2*np.pi/360, threshold=100)
+        lines = cv2.HoughLines(canny, rho=2, theta=2*np.pi/3600, threshold=100)
         # find all intersection points of the 10 most important lines
         maxshape = np.max(self.image.shape)
         l = []
@@ -150,7 +180,7 @@ class Calibration():
         self.bullseye.x = c[0][0]
         self.bullseye.y = c[1][0]
         if return_lines:
-            return m, angles
+            return {"multilinestring": m, "angles": angles}
 
     def __opt_rotation(self, phi: float, bullseye_rot: np.array, M1: np.ndarray) -> float:
         M2 = rotate_anchor(phi=phi, x=bullseye_rot[0], y=bullseye_rot[1])
@@ -160,33 +190,31 @@ class Calibration():
         # loss = squared difference between distances
         return np.diff(dists)[0]**2
 
-    def transform_to_circle(self, image: np.ndarray) -> Union[np.ndarray, np.ndarray]:
+    def transform_to_circle(self, image: np.ndarray, output_shape: Tuple[int]=(2000,2000), pad: int=0) -> Union[np.ndarray, np.ndarray]:
         assert self.ellipse.x, "Find ellipse first."
         assert self.bullseye.x, "Find Bull's Eye first."
-        # Step 1 - equalize ellipsis rotation
+        # step 1 - equalize ellipsis rotation
         M1 = self.ellipse.get_equalize_rotation_matrix()
-        # Step 2 - rotate ellipse, so that Bull's Eye is in the ellipse center calculate new bulleye coordinates and ellipse edge points
+        # step 2 - rotate ellipse, so that Bull's Eye is in the ellipse center calculate new bulleye coordinates and ellipse edge points
         bullseye_rot = self.bullseye.transform(M1)
         #   vectorize loss function
         opt_func = np.vectorize(lambda phi: self.__opt_rotation(phi, bullseye_rot=bullseye_rot, M1=M1))
-        res = minimize(opt_func, x0=0, bounds=[(-0.05,0.05)])
+        res = minimize(opt_func, x0=0, method="Nelder-Mead", bounds=[(-0.1,0.1)])
         if not res.success:
-            raise "Cannot rotate dartsboard correctly."
+            raise Exception(f"Cannot rotate dartsboard correctly. Fit results: {res}")
         #   result = best phi for rotation matrix M2
         M2 = rotate_anchor(phi=res.x[0], x=bullseye_rot[0], y=bullseye_rot[1])
-        # Step 3 - transform ellipse to circle
-        M3 = self.ellipse.get_ellipse_to_circle_rotation_matrix()
-        # Done.
-        M = M3@M2@M1
-        # Warp image with resulting transformation matrix
-        t_img = cv2.warpPerspective(self.ellipse_mask(image), M, (image.shape[:2][::-1]))
+        # done
+        M = M2@M1
+        # warp image with resulting transformation matrix
+        #t_img = cv2.warpPerspective(self.ellipse_mask(image), M, (image.shape[:2][::-1]))
+        t_img = cv2.warpPerspective(image, M, (image.shape[:2][::-1]))
         # find bounding box in grayscale image
-        gray = cv2.cvtColor(t_img.copy(),cv2.COLOR_BGR2GRAY) 
-        contours, hierarchy = cv2.findContours(gray, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)[-2:]
+        gray = cv2.cvtColor(t_img.copy(), cv2.COLOR_BGR2GRAY) 
+        contours, _ = cv2.findContours(gray, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
         bbx, bby, bbw, bbh = cv2.boundingRect(contours[-1])
         # crop to found bounding box and rescale to standard size
-        transformed_image = cv2.resize(t_img[bby:bby+bbh,bbx:bbx+bbw], dsize=(2000,2000), interpolation=cv2.INTER_CUBIC)
-
+        transformed_image = cv2.resize(t_img[bby-pad:bby+bbh+pad,bbx-pad:bbx+bbw+pad], dsize=output_shape, interpolation=cv2.INTER_CUBIC)
         return transformed_image, M
 
     def do(self) -> None:
